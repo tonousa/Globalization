b4f8922cf39a7d6dd85f0c51a745ed02b89965ec
tonousa <tonousa@hotmail.com>
2015-09-04 09:01:30 -0400

    1

---
+++ Globalization/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Globalization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Globalization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Demo> list = new List<Demo>();
+
+        public ActionResult Index()
+        {
+            return View(list);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Demo model)
+        {
+            if (ModelState.IsValid)
+            {
+                list.Add(model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Globalization/Models/Demo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Globalization.Models
+{
+    public class Demo
+    {
+        [DisplayName("Name")]
+        [Required(ErrorMessage="Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage="Age is required")]
+        public int Age { get; set; }
+    }
+}
